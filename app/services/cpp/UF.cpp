7a2ee685a540dfc9b9bdc437a4fd9bca4772bd6a
riccardompesce <riccardompesce@gmail.com>
2021-12-03 19:02:01 +0100

    implemented some union find methods

--- app/services/cpp/UF.cpp
+++ app/services/cpp/UF.cpp
@@ -2,3 +2,40 @@
 
 #include "UF.hpp"
 
+UnionFind::UnionFind(int n) 
+{
+    if (n < 0) {
+        throw std::invalid_argument("n must be equal or greater than 0");
+    }
+    this->count_ = n;
+    this->parent_ = std::vector<int>(n);
+    this->rank_ = std::vector<int>(n);
+    
+    for (int i = 0; i < n; i++) {
+        this->parent_[i] = i;
+        this->rank_[i] = 0;
+    }
+}
+
+void UnionFind::do_union(int p, int q)
+{
+    int root_p = this->do_find(p);
+    int root_q = this->do_find(q);
+
+    if (root_p == root_q) {
+        return;
+    }
+
+    if (this->rank_[root_p] < this->rank_[root_q]) {
+        this->parent_[root_p] = root_q;
+    }
+    else if (this->rank_[root_p] > this->rank_[root_q]) {
+        this->parent_[root_q] = root_p;
+    }
+    else {
+        this->parent_[root_q] = root_p;
+        this->rank_[root_p] += 1;
+    }
+
+    this->count_ -= 1;
+}
--- app/services/cpp/UF.hpp
+++ app/services/cpp/UF.hpp
@@ -1,4 +1,17 @@
+#include <vector>
+
 class UnionFind
 {
+    private:
+        std::vector<int> parent_;
+        std::vector<int> rank_;
+        int count_;
+    public:
+        UnionFind(int n);
+        ~UnionFind();
 
+        void do_union(int p, int q);
+        int do_find(int p);
+        bool are_connected(int p, int q);
+        int count();
 };
