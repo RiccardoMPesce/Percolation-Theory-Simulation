d1b4c52eb235f0905efafe708872b9f3b8ec08fa
riccardompesce <riccardompesce@gmail.com>
2021-12-03 20:48:35 +0100

    Commented out some code

--- app/services/cpp/UnionFind.cpp
+++ app/services/cpp/UnionFind.cpp
@@ -24,11 +24,13 @@ UnionFind::~UnionFind()
 }
 
 int UnionFind::do_find(int p)
-{
+{   
+    // Check that p is within the bounds of our underlying vector
     if (p < 0 || p >= this->parent_.size()) {
         throw std::invalid_argument("p out of bounds");
     }
 
+    // Finding the "root" for node p    
     while (p != this->parent_[p]) {
         this->parent_[p] = this->parent_[this->parent_[p]];
         p = this->parent_[p];
@@ -44,6 +46,7 @@ int UnionFind::count()
 
 bool UnionFind::are_connected(int p, int q)
 {
+    // They are connected if the root is the same element
     return this->do_find(p) == this->do_find(q);
 }
 
@@ -52,10 +55,13 @@ void UnionFind::do_union(int p, int q)
     int root_p = this->do_find(p);
     int root_q = this->do_find(q);
 
+    // The connection is trivial
     if (root_p == root_q) {
         return;
     }
 
+    // The root with the larger rank becomes the root of other one
+    // Basically, the lower rank root attaches the larger rank root
     if (this->rank_[root_p] < this->rank_[root_q]) {
         this->parent_[root_p] = root_q;
     }
