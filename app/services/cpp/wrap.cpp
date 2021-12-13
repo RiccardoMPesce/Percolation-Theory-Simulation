ab151b08d053f5f20153fed6f49f930e920aeed5
riccardompesce <riccardompesce@gmail.com>
2021-12-13 12:18:15 +0100

    Adds pip requirements file

--- app/services/cpp/wrap.cpp
+++ app/services/cpp/wrap.cpp
@@ -5,8 +5,8 @@
 
 namespace py = pybind11;
 
-PYBIND11_PLUGIN(wrap) {
-    py::module m("wrap", "Example");
+PYBIND11_MODULE(wrap, m) {
+    m.doc() = "Example plugin";
     m.def("compute_mean", &compute_mean<double>, "Desc");
     return m.ptr();
 }
