019095f39c22d049c0f6c53153b42f772dece6b5
riccardompesce <riccardompesce@gmail.com>
2021-12-18 20:02:31 +0100

    We don't need stat learning - we need data sources

--- app/app.py
+++ app/app.py
@@ -2,11 +2,13 @@ from fastapi import FastAPI
 
 from .routers import simulation
 from .routers import dataset
+from .routers import statistical_learning
 
 app = FastAPI()
 
 app.include_router(simulation.router)
 app.include_router(dataset.router)
+app.include_router(statistical_learning.router)
 
 @app.get("/")
 async def root():
