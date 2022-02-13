79c9cb35e7a13177e550568cd14797a1cf99ee5f
riccardompesce <riccardompesce@gmail.com>
2022-02-13 15:11:07 +0100

    Removed dataset as it is useless for our end

--- app/app.py
+++ app/app.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
 
 from .routers import simulation
-from .routers import dataset
 
 app = FastAPI()
 
 app.include_router(simulation.router)
-app.include_router(dataset.router)
 
 @app.get("/")
 async def root():
